8e2732a37a8f5387dab515fa60124c3aac21fd81
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2022-03-22 14:28:03 -0400

    commit

--- final_project.js
+++ final_project.js
@@ -1,12 +1,3 @@
-// These functions open and close the contact form
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
-
 // This function displays the first image in the slideshow when the page loads
 var slideIndex = 1;
 showSlides(slideIndex);
